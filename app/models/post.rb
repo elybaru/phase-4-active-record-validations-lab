0dc1c651c05bc26e1986110b6a3ba1ad38ff1dce
Ely Karr <elykarr@gmail.com>
2022-03-29 14:50:36 -0700

    commit tree 32a41fe5497fb5109153926c53d9ae21af321c8d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :summary, length: { maximum: 250 }
+    validates :content, length: { minimum: 250 }
+    validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+    validate :disallow_clickbait
+    
+    def disallow_clickbait
+        if title == "True Facts"
+            errors.add(:title, "Clickbait is not permitted")
+        end
+    end
 end
